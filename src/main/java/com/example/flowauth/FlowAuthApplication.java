8bd32fe85974dc5fafd11e688d94183302ce97df
steamedEggMaster <cksdud5014@naver.com>
2024-07-23 16:31:34 +0900

    initial setting

---
+++ src/main/java/com/example/flowauth/FlowAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flowauth/FlowAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
