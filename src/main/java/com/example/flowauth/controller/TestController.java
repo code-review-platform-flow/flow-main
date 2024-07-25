f68fc87005ceaa97de5a5733c4b0a626e6193b19
steamedEggMaster <cksdud5014@naver.com>
2024-07-25 18:08:29 +0900

    FLOW-52 Make TestController for test

--- src/main/java/com/example/flowauth/controller/TestController.java
+++ src/main/java/com/example/flowauth/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.flowauth.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    @CrossOrigin
     @GetMapping("/communication")
     public ResponseEntity<String> testCommunication(){
         return ResponseEntity.ok("good response");
