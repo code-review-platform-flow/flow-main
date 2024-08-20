67a5f9a8d607477c94821639268b753dc5bdb09e
gypark <a1061602@gmail.com>
2024-08-20 11:04:34 +0900

    feat: 팔로우 알람 개발

--- src/main/java/com/flow/main/service/follows/FollowsFollowService.java
+++ src/main/java/com/flow/main/service/follows/FollowsFollowService.java
@@ -3,8 +3,12 @@ package com.flow.main.service.follows;
 import com.flow.main.dto.controller.follow.follow.request.FollowRequestDto;
 import com.flow.main.dto.controller.follow.follow.response.FollowResponseDto;
 import com.flow.main.dto.jpa.follows.FollowsDto;
+import com.flow.main.dto.jpa.userinfo.UserInfoDto;
 import com.flow.main.dto.jpa.users.UsersDto;
+import com.flow.main.event.alarm.AlarmEvent;
+import com.flow.main.event.alarm.AlarmEventPublisher;
 import com.flow.main.service.follows.persistence.FollowsService;
+import com.flow.main.service.userinfo.persistence.UserInfoService;
 import com.flow.main.service.users.persistence.UsersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,11 +19,14 @@ public class FollowsFollowService {
 
     private final UsersService usersService;
     private final FollowsService followsService;
+    private final UserInfoService userInfoService;
+    private final AlarmEventPublisher alarmEventPublisher;
 
     public FollowResponseDto follow(FollowRequestDto followRequestDto){
 
         UsersDto follower = usersService.findByEmail(followRequestDto.getFollowerEmail());
         UsersDto followee = usersService.findByEmail(followRequestDto.getFolloweeEmail());
+        UserInfoDto userInfoDto = userInfoService.findByUserId(follower.getUserId());
 
         followsService.checkAlreadyFollowed(follower.getUserId(), followee.getUserId());
 
@@ -28,6 +35,16 @@ public class FollowsFollowService {
         followsDto.setFolloweeId(followee.getUserId());
 
         FollowsDto savedFollowsDto = !followsDto.isUseYn() ? followsService.reuse(followsDto) : followsService.save(followsDto);
+
+        // 알람 이벤트 발행
+        alarmEventPublisher.alarm(AlarmEvent.builder()
+            .userId(followee.getUserId()) // 팔로위
+            .alarmType("FOLLOW")
+            .message(userInfoDto.getUserName() + "님이 팔로우 했습니다")
+            .isRead(false)
+            .referenceId(followsDto.getFollowerId())
+            .referenceTable("follows")
+            .build());
 
         return FollowResponseDto.builder()
             .followId(savedFollowsDto.getFollowId())
