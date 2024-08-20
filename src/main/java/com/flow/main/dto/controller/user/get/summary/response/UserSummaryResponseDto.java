214435da42562c820b3b413f30ea15449de102e3
steamedEggMaster <cksdud5014@naver.com>
2024-08-21 01:45:14 +0900

    FLOW-164, /user/summary 구현 성공, test 완료

---
+++ src/main/java/com/flow/main/dto/controller/user/get/summary/response/UserSummaryResponseDto.java
@@ -0,0 +1,21 @@
+package com.flow.main.dto.controller.user.get.summary.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserSummaryResponseDto {
+
+    private String profileUrl;
+    private String userName;
+    private String majorName;
+    private String studentNumber;
+
+}
---
+++ src/main/java/com/flow/main/service/userinfo/UserInfoSummaryService.java
@@ -0,0 +1,35 @@
+package com.flow.main.service.userinfo;
+
+import com.flow.main.dto.controller.user.get.summary.request.UserSummaryRequestDto;
+import com.flow.main.dto.controller.user.get.summary.response.UserSummaryResponseDto;
+import com.flow.main.dto.jpa.major.MajorDto;
+import com.flow.main.dto.jpa.userinfo.UserInfoDto;
+import com.flow.main.dto.jpa.users.UsersDto;
+import com.flow.main.service.major.persistence.MajorService;
+import com.flow.main.service.userinfo.persistence.UserInfoService;
+import com.flow.main.service.users.persistence.UsersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserInfoSummaryService {
+
+    private final UsersService usersService;
+    private final UserInfoService userInfoService;
+    private final MajorService majorService;
+
+    public UserSummaryResponseDto getUserSummary(UserSummaryRequestDto userSummaryRequestDto){
+        UsersDto usersDto = usersService.findByEmail(userSummaryRequestDto.getEmail());
+        UserInfoDto userInfoDto = userInfoService.findByUserId(usersDto.getUserId());
+        MajorDto majorDto = majorService.findByMajorId(userInfoDto.getMajorId());
+
+        return UserSummaryResponseDto.builder()
+            .profileUrl(userInfoDto.getProfileUrl())
+            .userName(userInfoDto.getUserName())
+            .studentNumber(userInfoDto.getStudentNumber())
+            .majorName(majorDto.getMajorName())
+            .build();
+    }
+
+}
